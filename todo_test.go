0800315751ab3ff395542c13eb011de70716ad8a
Todd Crone <twcrone@gmail.com>
2021-07-06 22:00:02 -0400

    Test delete

--- todo_test.go
+++ todo_test.go
@@ -32,3 +32,28 @@ func TestComplete(t *testing.T) {
 		t.Errorf("New task should be completed")
 	}
 }
+
+func TestDelete(t *testing.T) {
+	l := todo.List{}
+	tasks := []string{
+		"New Task 1",
+		"New Task 2",
+		"New Task 3",
+	}
+	for _, v := range tasks {
+		l.Add(v)
+	}
+	if l[0].Task != tasks[0] {
+		t.Errorf("Expected %q, got %q instead", tasks[0], l[0].Task)
+	}
+	err := l.Delete(2)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if len(l) != 2 {
+		t.Errorf("Expected list length %d, got %d instead", 2, len(l))
+	}
+	if l[1].Task != tasks[2] {
+		t.Errorf("Expected %q, got %q instead", tasks[2], l[1].Task)
+	}
+}
